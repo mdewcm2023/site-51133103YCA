05d5753057164bc787ada87febb43ac5931ae05b
51133103 <51133103@nfu.edu.tw>
2023-03-17 11:19:30 +0800

    更改首頁信息

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'HI', 'tags': '', 'url': 'About.html'}]};
