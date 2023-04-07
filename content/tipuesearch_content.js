c7c00dbc83efd6bd12a744e0c91e83ff190010e0
51133103 <51133103@nfu.edu.tw>
2023-04-07 09:20:41 +0800

    更改標題

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'First', 'text': '51133103 \n 51133103 \n', 'tags': '', 'url': 'First.html'}]};
+var tipuesearch = {"pages": [{'title': '51133103', 'text': '51133103 \n 51133103 \n \n', 'tags': '', 'url': '51133103.html'}]};
